d48eb8b5ec0ab4d967267cc62ec09516aca79269
Niklas Andersson <niklasandersson@Niklass-MacBook-Pro.local>
2014-01-08 22:51:24 +0100

    Initial commit from local git

---
+++ TempGitTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TempGitTest
+//
+//  Created by Niklas Andersson on 08/01/14.
+//  Copyright (c) 2014 Niklas Andersson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TempGitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TempGitTest
+//
+//  Created by Niklas Andersson on 08/01/14.
+//  Copyright (c) 2014 Niklas Andersson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
