f8eb005b1f3ab143141b8d7bcadf81c5c27c06be
Niklas Andersson <niklasandersson@Niklass-MacBook-Pro.local>
2014-01-09 07:07:40 +0100

    add

---
+++ TempGitTest/DataModel.h
@@ -0,0 +1,13 @@
+//
+//  DataModel.h
+//  TempGitTest
+//
+//  Created by Niklas Andersson on 09/01/14.
+//  Copyright (c) 2014 Niklas Andersson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataModel : NSObject
+
+@end
---
+++ TempGitTest/DataModel.m
@@ -0,0 +1,13 @@
+//
+//  DataModel.m
+//  TempGitTest
+//
+//  Created by Niklas Andersson on 09/01/14.
+//  Copyright (c) 2014 Niklas Andersson. All rights reserved.
+//
+
+#import "DataModel.h"
+
+@implementation DataModel
+
+@end
